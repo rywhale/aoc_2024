546ea8c31da23ba6f8f26d112442b1158f443a89
rywhale <rdgwhaley@gmail.com>
2024-12-16 11:42:18 -0500

    Day 11 part 1

---
+++ day11/part1.R
@@ -0,0 +1,40 @@
+input <- "92 0 286041 8034 34394 795 8 2051489" |> 
+# input <- "125 17" |>
+  stringr::str_split_1(" ") |> 
+  as.numeric()
+
+stone_it <- function(stone){
+  if(stone == 0){
+    1
+  }else if(nchar(stone) %% 2 == 0){
+    
+    if(nchar(stone) == 2){
+      first <- substr(stone, 1, 1)
+      second <- substr(stone, 2, 2)
+    }else{
+      first <- substr(stone, 1, nchar(stone) / 2)
+      second <- substr(stone, nchar(stone) / 2 + 1, nchar(stone))
+    }
+    
+    as.numeric(c(first, second))
+  }else{
+    stone * 2024
+  }
+  
+}
+
+blinks <- 0
+stones <- input
+
+while(blinks < 25){
+  message("You've blinked ", blinks, " times")
+  stones <- stones |> 
+    purrr::map(
+      stone_it
+    ) |> 
+    unlist()
+  
+  blinks <- blinks + 1
+}
+
+message("Total stones: ", length(stones))
